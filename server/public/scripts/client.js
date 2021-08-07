d7abd32f348ee2aa02420ec1317d31ceb45d412d
Dan St. Aubin <staubindj@gmail.com>
2021-08-07 11:58:25 -0500

    resolved conflict between local jQuery and bootstrap jQuery, corrected client side createTask

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -16,7 +16,7 @@ function createTask() {
     // collect info
     let task = $('#taskInput').val();
     // empty the input area
-    $('#taskInput').empty();
+    $('#taskInput').val('');
     // send to server
     $.ajax({
         method: 'POST',
@@ -24,6 +24,7 @@ function createTask() {
         data: {task}
     }).then((response) => {
         console.log('Successfully posted new task: ', response);
+        // getTasks();
     }).catch((error) => {
         console.log('Failed to post new task: ', error);
         alert('Failed to post new task. Check console for error.')
--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -5,7 +5,11 @@ const router = new express.Router();
 // GET
 
 // POST
-
+router.post('/', (req, res) => {
+    console.log('server for post');
+    let newTask = req.body.task;
+    console.log(newTask);
+})
 // PUT
 
 // DELETE
